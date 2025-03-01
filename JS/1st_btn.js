085d4ac30cf7c48235ba061cbe61f8cd20f86245
shafayatGit <shafayathossain.drmc@gmail.com>
2025-03-01 12:58:53 +0600

    btn upadated

---
+++ JS/1st_btn.js
@@ -0,0 +1,26 @@
+document.getElementById('id_1stCard').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    alert('Board Updated Successfully');
+
+    // Adding Task Text
+    const taskText = document.getElementById('task_text').innerText
+    const IntTaskText = parseInt(taskText);
+
+    const newTaskText = IntTaskText - 1;
+    
+    document.getElementById('task_text').innerText = newTaskText;
+
+    // Adding Nav Text
+    const navText = document.getElementById('nav_text').innerText
+    const IntNavText = parseInt(navText);
+
+    const newNavText = IntNavText + 1;
+
+    document.getElementById('nav_text').innerText = newNavText;
+
+    const firstCardBtn = document.getElementById('id_1stCard');
+    firstCardBtn.disabled = true;
+    firstCardBtn.style.backgroundColor='slategray'
+
+})
